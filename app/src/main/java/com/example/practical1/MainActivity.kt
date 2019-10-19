a3ae36a79a4ecbe8612d97191f48341f93d87baa
FelixCATus <vcheehong@gmail.com>
2019-10-19 18:18:29 +0800

    count up (challenge) done

--- app/src/main/java/com/example/practical1/MainActivity.kt
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.practical1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,10 +15,30 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+        //Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        if (resultText.text == "Hello World!"){
+            resultText.text = "1"
+        }else {
+            var resultInt = resultText.text.toString().toInt()
+            if (resultText.text != "6") {
+                resultText.text = resultInt.inc().toString()
+            }
+            else{
+                Toast.makeText(this, "Number has reached 6!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
