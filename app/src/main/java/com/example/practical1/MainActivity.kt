cfddb6542ddffc21275965d46d2af23947100aba
FelixCATus <vcheehong@gmail.com>
2019-10-23 09:20:18 +0800

    diceImage done

--- app/src/main/java/com/example/practical1/MainActivity.kt
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -3,42 +3,114 @@ package com.example.practical1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
         val countUpButton: Button = findViewById(R.id.count_up_button)
         countUpButton.setOnClickListener { countUp() }
+
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
         //Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+        val drawableResource1 = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage1.setImageResource(drawableResource1)
+
+        val drawableResource2 = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage2.setImageResource(drawableResource2)
     }
 
     private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        if (resultText.text == "Hello World!"){
-            resultText.text = "1"
-        }else {
-            var resultInt = resultText.text.toString().toInt()
-            if (resultText.text != "6") {
-                resultText.text = resultInt.inc().toString()
-            }
-            else{
-                Toast.makeText(this, "Number has reached 6!", Toast.LENGTH_SHORT).show()
-            }
-        }
+        val diceState = diceImage.drawable.constantState
+        val diceState1 = diceImage1.drawable.constantState
+        val diceState2 = diceImage2.drawable.constantState
+
+        if(diceState == resources.getDrawable(R.drawable.empty_dice).constantState)
+            diceImage.setImageResource(R.drawable.dice_1)
+        else if(diceState == resources.getDrawable(R.drawable.dice_1).constantState)
+            diceImage.setImageResource(R.drawable.dice_2)
+        else if(diceState == resources.getDrawable(R.drawable.dice_2).constantState)
+            diceImage.setImageResource(R.drawable.dice_3)
+        else if(diceState == resources.getDrawable(R.drawable.dice_3).constantState)
+            diceImage.setImageResource(R.drawable.dice_4)
+        else if(diceState == resources.getDrawable(R.drawable.dice_4).constantState)
+            diceImage.setImageResource(R.drawable.dice_5)
+        else
+            diceImage.setImageResource(R.drawable.dice_6)
+
+        if(diceState1 == resources.getDrawable(R.drawable.empty_dice).constantState)
+            diceImage1.setImageResource(R.drawable.dice_1)
+        else if(diceState1 == resources.getDrawable(R.drawable.dice_1).constantState)
+            diceImage1.setImageResource(R.drawable.dice_2)
+        else if(diceState1 == resources.getDrawable(R.drawable.dice_2).constantState)
+            diceImage1.setImageResource(R.drawable.dice_3)
+        else if(diceState1 == resources.getDrawable(R.drawable.dice_3).constantState)
+            diceImage1.setImageResource(R.drawable.dice_4)
+        else if(diceState1 == resources.getDrawable(R.drawable.dice_4).constantState)
+            diceImage1.setImageResource(R.drawable.dice_5)
+        else
+            diceImage1.setImageResource(R.drawable.dice_6)
+
+        if(diceState2 == resources.getDrawable(R.drawable.empty_dice).constantState)
+            diceImage2.setImageResource(R.drawable.dice_1)
+        else if(diceState2 == resources.getDrawable(R.drawable.dice_1).constantState)
+            diceImage2.setImageResource(R.drawable.dice_2)
+        else if(diceState2 == resources.getDrawable(R.drawable.dice_2).constantState)
+            diceImage2.setImageResource(R.drawable.dice_3)
+        else if(diceState2 == resources.getDrawable(R.drawable.dice_3).constantState)
+            diceImage2.setImageResource(R.drawable.dice_4)
+        else if(diceState2 == resources.getDrawable(R.drawable.dice_4).constantState)
+            diceImage2.setImageResource(R.drawable.dice_5)
+        else
+            diceImage2.setImageResource(R.drawable.dice_6)
+    }
+
+    private fun reset(){
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage1.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
     }
 }
